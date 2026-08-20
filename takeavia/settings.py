bad1e31428171a56fc2d3e23d37a4d29ab421d8c
Takeavia Server <admin@takeavia.uz>
2026-08-20 13:45:51 +0000

    Production uchun LOGGING sozlamasi qo'shildi

--- takeavia/settings.py
+++ takeavia/settings.py
@@ -192,3 +192,16 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {'console': {'class': 'logging.StreamHandler'}},
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
